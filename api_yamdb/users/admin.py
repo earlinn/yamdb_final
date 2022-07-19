c2b41af5eca38e49b4aee372a00ee0e5dc590f94
Galina Volkova <earlinndrow@gmail.com>
2022-07-19 19:07:06 +0300

    Customize users display in admin panel

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    """Class to customize Users display in admin panel."""
+    list_display = [
+        'pk', 'username', 'email', 'role', 'first_name', 'last_name', 'bio']
+    search_fields = ['username', 'first_name', 'last_name', 'email']
+    list_filter = ['role']
+    empty_value_display = '-empty-'
+
+
+admin.site.register(User, UserAdmin)
